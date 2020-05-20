6328395437e73e7b1fc5561ee8ea109fa19ae1d4
Kasper <kasperhoffmann@hotmail.dk>
2020-05-20 08:20:19 +0200

    Make app that chooses random apex hero

---
+++ RandomApexHero/RandomApexHero/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RandomApexHero
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string[] heroes = { "Bloodhound", "Gibralta", "Bangalore", "Caustic", "Crypto", "Lifeline", "Loba", "Mirage", "Octane", "Pathfinder", "Revenant", "Wattson", "Wraith" };
+
+        public string pickRandomHero()
+        {
+            Random rand = new Random();
+
+            return heroes[rand.Next(0, 13)];
+
+
+        }
+
+        public void randomHeroButtonClicked(object sender, EventArgs args)
+        {
+            randomHeroLabel.Text = pickRandomHero();
+        }
+
+
+    }
+}
